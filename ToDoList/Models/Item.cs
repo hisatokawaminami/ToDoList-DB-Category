d3dfd6f49e59721f9b3d2701f55747d4b9583460
Austin Barr and Hisato Kawaminami <austin@>
2018-07-10 11:41:54 -0700

    add everything

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,15 +1,37 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+using System;
 
 namespace ToDoList.Models
 {
   public class Item
   {
+    private int _id;
     private string _description;
-    private static List<Item> _instances = new List<Item> {};
 
-    public Item (string description)
+    public Item (string Description, int Id = 0)
     {
-      _description = description;
+      _id = Id;
+      _description = Description;
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+        Item newItem = (Item) otherItem;
+        bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+        return (descriptionEquality);
+      }
+    }
+    public override int GetHashCode()
+    {
+      return this.GetDescription().GetHashCode();
     }
 
     public string GetDescription()
@@ -21,17 +43,54 @@ namespace ToDoList.Models
       _description = newDescription;
     }
 
-    public static List<Item> GetAll()
+    public int GetId()
     {
-      return _instances;
+      return _id;
     }
+
     public void Save()
     {
-      _instances.Add(this);
+      
     }
-    public static void ClearAll()
+
+    public static void DeleteAll()
     {
-      _instances.Clear();
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+        {
+          conn.Dispose();
+        }
+        return allItems;
     }
   }
 }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -6,36 +6,41 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ToDoList
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-          app.UseDeveloperExceptionPage();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+      var builder = new ConfigurationBuilder()
+      .SetBasePath(env.ContentRootPath)
+      .AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
+
+    public IConfigurationRoot Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}");
+        });
+        app.Run(async (context) =>
+        {
+          await context.Response.WriteAsync("Hello World!");
+          });
+        }
+
+      }
+      public static class DBConfiguration
+      {
+        public static string ConnectionString = "server=localhost; user id=root;password=root;port=8889;database=to_do;";
+      }
 }
